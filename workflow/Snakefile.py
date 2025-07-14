a456a9c22069738e2c10790d5d0a67530c82471d
Sara Doina <saradoina.schuetz@bluewin.ch>
2025-07-14 10:06:25 +0200

    Add files via upload

--- workflow/Snakefile.py
+++ workflow/Snakefile.py
@@ -1,6 +1,6 @@
-####################################################################################################
-##                             Snakemake pipeline for Genome analysis                             ##
-####################################################################################################
+#############################################################################################
+##                        Snakemake pipeline for Genome analysis                           ##
+#############################################################################################
 
 ########################################
 ## To run the pipeline use this command#
@@ -19,19 +19,20 @@ import pandas as pd
 import glob
 import os
 
-
 ########################################
 ## Configuration                      ##
 ########################################
 
-configfile: "../config/config.yaml" # where to find parameters
+configfile: "../config/config.yaml" 
 
-WORKING_DIR     = config["working_dir"]
-OUTPUT_DIR      = config["output_dir"]
-DATABASE_DIR    = config["database_dir"]
-PROJECT_DIR     = config["project_dir"]
-PROJECT_NAME    = config["project_name"]
-ASSEMBLER       = config.get("assembler", "spades")
+WORKING_DIR         = config["working_dir"]
+OUTPUT_DIR          = config["output_dir"]
+DATABASE_DIR        = config["database_dir"]
+CGMLST_SCHEMA_DIR   = config["cgmlst_schema"] 
+PROJECT_DIR         = config["project_dir"]
+PROJECT_NAME        = config["project_name"]
+ASSEMBLER           = config["assembler"]
+ANNOTATOR           = config["annotator"]
 
 ########################################
 ## Samples                            ##
@@ -40,27 +41,21 @@ ASSEMBLER       = config.get("assembler", "spades")
 ## Raw read data
 SAMPLES         = config["samples"]
 
-
-## Additional genome data, for example downloaded from NCBI
-GENOMES_DIR     = PROJECT_DIR + "ncbi_dataset/data"
-
-fna_files = glob.glob(os.path.join(GENOMES_DIR, "*", "*.fna"))
-genomes_map = {}
-for f in fna_files:
-    folder = os.path.basename(os.path.dirname(f))           # GCA_000236925.1
-    basename = os.path.basename(f).replace("_genomic.fna", "")  # GCA_000236925.1_ASM23692v1
-    genomes_map[folder] = basename
-
-GENOMES         = sorted(genomes_map.keys())
+## External genomes 
+GENOMES         = config["genomes"]
 
 ALL_GENOMES     = sorted(set(SAMPLES + GENOMES))
 
 # Wähle erstes als Referenz
-REF_SAMPLE = ALL_GENOMES[0]
+REF_SAMPLE      = config["refg"]
+
+GWAS_GENOMES = [g for g in ALL_GENOMES if g != REF_SAMPLE]
 
 print("SAMPLES:", SAMPLES) 
 print("GENOMES:", GENOMES)
 print("ALL_GENOMES:", ALL_GENOMES)
+print("REF_SAMPLE:", REF_SAMPLE)
+print("GWAS_GENOMES:", GWAS_GENOMES)
 
 ########################################
 ## Rules                              ##
@@ -78,67 +73,118 @@ elif ASSEMBLER == "unicycler":
     include: "rules/unicycler.smk"
 else:
     raise ValueError(f"Unknown assembler specified: {ASSEMBLER}")
-#include: "rules/fix_contigs.smk"
 include: "rules/quast.smk"
 
 # Copy external genomes
-include: "rules/copy_ncbi_data.smk"
+include: "rules/copy_genomes.smk"
 
 ## Annotation
-include: "rules/prokka.smk"
-include: "rules/copy_prokka_to_temp.smk"
+if ANNOTATOR == "prokka":
+    include: "rules/prokka.smk"
+elif ANNOTATOR == "bakta":
+    include: "rules/bakta.smk"
+else:
+    raise ValueError(f"Unknown aannotator specified: {ANNOTATOR}")
 include: "rules/emapper_kegganog.smk"
-include: "rules/copy_emapper_to_temp.smk"
 
-# SNP analysis 
-include: "rules/copy_reference.smk"
-include: "rules/refg_prokka.smk"
-include: "rules/snippy.smk"
+
+# QC summary
+include: "rules/summarize_genomes.smk" 
 
 # Pangenome analysis
 include: "rules/pirate.smk"
-#include: "rules/anvio.smk"
+include: "rules/anvio.smk"
 
 # Typing
-include: "rules/mlst.smk"
-include: "rules/spaTyper.smk"
-# coregenomeMLST
+include: "rules/mlst_spatyper.smk"
+include: "rules/chewbacca.smk" 
 
 # AMR/virulence genes screening
 include: "rules/abricate.smk"
 
+# SNP analysis 
+include: "rules/refg_gbk.smk"
+include: "rules/snippy.smk"
+
+# GWAS analysis 
+include: "rules/gwas.smk"
+
+# Mapping
+include: "rules/align_reads_to_reference.smk"
 
 ########################################
 ## Desired outputs                    ##
 ########################################
 
-MULTIQC                     = [OUTPUT_DIR + "qc/multiqc/multiqc_report.html"]
-FASTP_OUTPUT                = expand(OUTPUT_DIR + "qc/fastp/{sample}_trimmed_R1.fastq.gz", sample=SAMPLES)
-CONTIGS                     = expand(OUTPUT_DIR + "{sample}/assembly/contigs.fasta", sample=SAMPLES)
-FIX_CONTIGS                 = expand(OUTPUT_DIR + "{sample}/assembly/contigs_fixed.fasta", sample=SAMPLES)
-QUAST                       = expand(OUTPUT_DIR + "qc/quast/{sample}/report.html", sample=SAMPLES)
-NCBI_GENOMES                = expand(WORKING_DIR + "genomes/{genome}.fasta", genome=GENOMES) 
-NCBI_CONTIGS                = expand(OUTPUT_DIR + "{genome}/assembly/contigs_fixed.fasta", genome=GENOMES) 
-PROKKA                      = expand(OUTPUT_DIR + "{all_genomes}/annotation/prokka/", all_genomes=ALL_GENOMES)
-EMAPPER                     = expand(OUTPUT_DIR + "{all_genomes}/annotation/emapper/", all_genomes=ALL_GENOMES)
-COPY_DEC_GFF                = expand(OUTPUT_DIR + "temp_genome_annotaions_gff/{all_genomes}.emapper.decorated.gff", all_genomes=ALL_GENOMES)
-KEGGANOG                    = [OUTPUT_DIR + "kegganog/heatmap_figure.png"]
-COPY_GFF                    = expand(OUTPUT_DIR + "temp_genome_gff/{all_genomes}.gff", all_genomes=ALL_GENOMES)
-PIRATE                      = [OUTPUT_DIR + "pangenome/pirate/core_alignment.fasta"]
-PAN_TREE                    = [OUTPUT_DIR + "pangenome/iqtree/iqtree.log"]
-REFG_GBK                    = [WORKING_DIR + "reference/refg.gbk"]
-SNIPPY                      = expand(OUTPUT_DIR + "variant_calling/snippy/{all_genomes}/snps.vcf", all_genomes=ALL_GENOMES)
-COPY_VCF                    = expand(OUTPUT_DIR + "temp_genome_vcf/{all_genomes}.snps.vcf", all_genomes=ALL_GENOMES)
-SNIPPY_CORE                 = [OUTPUT_DIR + "variant_calling/snippy-core/core.full.aln"]
-CORE_TREE                   = [OUTPUT_DIR + "variant_calling/snippy-core/iqtree.log"]
-VCF_HEATMAP                 = [OUTPUT_DIR + "variant_calling/vcf_viewer/heatmap_output.html"]
-MLST                        = [OUTPUT_DIR + "typing/mlst/mlst.tsv"]
-SPATYPER                    = expand(OUTPUT_DIR + "typing/spaTyper/{all_genomes}_spatype.txt", all_genomes=ALL_GENOMES) 
-AMR_NCBI                    = expand(OUTPUT_DIR + "amr/{all_genomes}.ncbi.tsv", all_genomes=ALL_GENOMES) 
-#AMR_TABLE                   = [OUTPUT_DIR + "amr/resistance_summary.tsv"]
-#AMR_HEATMAP                = [OUTPUT_DIR + "amr/heatmap.png"]
+# Assembly and QC 
+QC_MULTIQC                      = [OUTPUT_DIR + "01_qc/multiqc/multiqc_report.html"]
+QC_FASTP                        = expand(OUTPUT_DIR + "01_qc/fastp/{sample}_trimmed_R1.fastq.gz", sample=SAMPLES)
+ASSEMBLY_CONTIGS                = expand(OUTPUT_DIR + "08_temp/temp_fasta/{sample}.fasta", sample=SAMPLES)
+QC_QUAST                        = expand(OUTPUT_DIR + "01_qc/quast/{sample}/report.txt", sample=SAMPLES)
+
+# Copy external genomes
+COPY_EXTERNAL_GENOMES           = expand(OUTPUT_DIR + "08_temp/temp_fasta/{genome}.fasta", genome=GENOMES)
+
+# Annotation PROKKA
+ANNOTATION_PROKKA               = expand(OUTPUT_DIR + "{all_genomes}/annotation/prokka/", all_genomes=ALL_GENOMES)
+ANNOTATION_PROKKA_COPY_GFF      = expand(OUTPUT_DIR + "08_temp/temp_gff/{all_genomes}.gff", all_genomes=ALL_GENOMES)
+ANNOTATION_PROKKA_COPY_TXT      = expand(OUTPUT_DIR + "{all_genomes}/annotation/prokka/prokka.txt", all_genomes=ALL_GENOMES)
+
+# QC summary 
+QC_SUMMARY                      = [OUTPUT_DIR + "01_qc/summary_qc.tsv"] 
+
+# Annotation KEGGANOG/EMAPPER
+ANNOTATION_EMAPPER              = expand(OUTPUT_DIR + "{all_genomes}/annotation/emapper/", all_genomes=ALL_GENOMES)
+KEGGANOG                        = [OUTPUT_DIR + "02_kegganog/figure_kegganog.png"]
+ANNOTATION_COPY_DEC_GFF         = expand(OUTPUT_DIR + "08_temp/temp_decorated_gff/{all_genomes}.emapper.decorated.gff", all_genomes=ALL_GENOMES)
 
 
+# Pangenome analysis PIRATE 
+PANGENOME_PIRATE                = [OUTPUT_DIR + "03_pangenome/pirate/core_alignment.fasta"]
+PANGENOME_TREE                  = [OUTPUT_DIR + "03_pangenome/pirate/core_alignment.fasta.treefile"]
+PANGENOME_CORE_GENES_DISTANCE   = [OUTPUT_DIR + "03_pangenome/pirate/core_gene_snp_distances.tsv"]
+
+# Typing
+TYPING_MLST                     = [OUTPUT_DIR + "06_typing/mlst/mlst.tsv"]
+TYPING_SPATYPER                 = expand(OUTPUT_DIR + "06_typing/spaTyper/{all_genomes}_spatype.txt", all_genomes=ALL_GENOMES)
+CGMLST_PREP_EXTERNAL_DB         = [CGMLST_SCHEMA_DIR + "PreparedSchema/schema.json"] 
+CGMLST_ALLELE_CALL              = [OUTPUT_DIR + "06_typing/chewbbaca/results_allele_call.json"]
+
+# AMR/virulence genes screening 
+AMR_NCBI                        = expand(OUTPUT_DIR + "07_amr/{all_genomes}.ncbi.tsv", all_genomes=ALL_GENOMES) 
+AMR_NCBI_SUMMARY                = [OUTPUT_DIR + "07_amr/amr_ncbi_summary.tab"]
+
+
+# Pangenome analysis ANVI'O 
+ANVIO_FASTA_REFORMATTED         = expand(OUTPUT_DIR + "08_temp/temp_fasta/{all_genomes}.simplified.fasta", all_genomes=ALL_GENOMES) 
+ANVIO_GENE_CALLS                = expand(OUTPUT_DIR + "03_pangenome/anvio/db/{all_genomes}_gene_calls.txt", all_genomes=ALL_GENOMES) 
+ANVIO_ANNOTATION                = expand(OUTPUT_DIR + "03_pangenome/anvio/db/{all_genomes}_gene_annot.txt", all_genomes=ALL_GENOMES)
+ANVIO_DB                        = expand(OUTPUT_DIR + "03_pangenome/anvio/db/{all_genomes}.db", all_genomes=ALL_GENOMES) 
+ANVIO_DONE_DB                   = expand(OUTPUT_DIR + "03_pangenome/anvio/db/{all_genomes}.annotation.done", all_genomes=ALL_GENOMES) 
+ANVIO_EXTERNAL_GENOMES_FILE     = [OUTPUT_DIR + "03_pangenome/anvio/external-genomes.txt"]
+ANVIO_STORAGE_DB                = [OUTPUT_DIR + "03_pangenome/anvio/anvio_storage-GENOMES.db"]
+ANVIO_PANGENOME_DB              = [OUTPUT_DIR + "03_pangenome/anvio/anvio_Pangenome-PAN.txt"]
+ANVIO_ANI                       = [OUTPUT_DIR + "03_pangenome/anvio/ANI/genome_similarity.txt"]
+
+
+# Variant calling SNIPPY 
+VC_REFG_GBK                     = [WORKING_DIR + "reference/refg.gbk"]
+VC_SNIPPY                       = expand(OUTPUT_DIR + "04_variant_calling/snippy/{all_genomes}/snps.vcf", all_genomes=ALL_GENOMES)
+VC_COPY_VCF                     = expand(OUTPUT_DIR + "08_temp/temp_vcf/{all_genomes}.snps.vcf", all_genomes=ALL_GENOMES)
+VC_COPY_VCF_GZ                  = expand(OUTPUT_DIR + "08_temp/temp_vcf_gz/{all_genomes}.snps.vcf.gz", all_genomes=ALL_GENOMES) 
+VC_SNIPPY_CORE                  = [OUTPUT_DIR + "04_variant_calling/snippy-core/core.full.aln"]
+VC_CORE_TREE                    = [OUTPUT_DIR + "04_variant_calling/snippy-core/iqtree.log"]
+VC_VCF_HEATMAP                  = [OUTPUT_DIR + "04_variant_calling/vcf_viewer/heatmap_output.html"]
+VC_SNP_DISTANCE                 = [OUTPUT_DIR + "04_variant_calling/snippy-core/snp_distance_matrix.tsv"]
+
+# GWAS 
+CSI                             = expand(OUTPUT_DIR + "08_temp/temp_vcf_gz/{gwas_genomes}.snps.vcf.gz.csi", gwas_genomes=GWAS_GENOMES) 
+VCF_MERGED                      = [OUTPUT_DIR + "05_gwas/merged.vcf.gz"]
+PHYLO_DIST                      = [OUTPUT_DIR + "05_gwas/phylogeny_dists.tsv"]
+
+
+# Mapping
+BAM                             = expand(OUTPUT_DIR + "{all_genomes}/mapping/{all_genomes}.sorted.bam", all_genomes=ALL_GENOMES)
 
 ########################################
 ## Pipeline                           ##
@@ -146,30 +192,50 @@ AMR_NCBI                    = expand(OUTPUT_DIR + "amr/{all_genomes}.ncbi.tsv", 
 
 rule all:
     input:
-        MULTIQC,
-        FASTP_OUTPUT,
-        CONTIGS,
-        FIX_CONTIGS,
-        QUAST, 
-        NCBI_GENOMES, 
-        NCBI_CONTIGS,
-        PROKKA,
-        COPY_GFF,
-        PIRATE,
-        PAN_TREE,
-        MLST,
-        SPATYPER,
-        AMR_NCBI, 
-        #AMR_TABLE,
-        ### Optional steps depending on config
-        *(EMAPPER if config["run"].get("emapper_kegganog") else []),
-        *(COPY_DEC_GFF if config["run"].get("emapper_kegganog") else []),
+        #QC_MULTIQC,
+        ASSEMBLY_CONTIGS,
+        QC_QUAST,
+        COPY_EXTERNAL_GENOMES, 
+        ANNOTATION_PROKKA,
+        ANNOTATION_PROKKA_COPY_GFF, 
+        ##### Optional steps depending on config emapper_kegganog = true
+        *(ANNOTATION_EMAPPER if config["run"].get("emapper_kegganog") else []),
+        *(ANNOTATION_COPY_DEC_GFF if config["run"].get("emapper_kegganog") else []), 
         *(KEGGANOG if config["run"].get("emapper_kegganog") else []),
-        *(REFG_GBK if config["run"].get("snippy") else []),
-        *(SNIPPY if config["run"].get("snippy") else []),
-        *(COPY_VCF if config["run"].get("snippy") else []), 
-        *(SNIPPY_CORE if config["run"].get("snippy") else []),
-        *(CORE_TREE if config["run"].get("snippy") else []),
-        *(VCF_HEATMAP if config["run"].get("snippy_vcf_heatmap") else []),
+        QC_SUMMARY, 
+        PANGENOME_PIRATE,
+        PANGENOME_TREE, 
+        PANGENOME_CORE_GENES_DISTANCE, 
+        TYPING_MLST,
+        TYPING_SPATYPER,
+        ### Optional outputs depending on config cgmlst = true 
+        *(CGMLST_PREP_EXTERNAL_DB if config["run"].get("cgmlst") else []), 
+        *(CGMLST_ALLELE_CALL if config["run"].get("cgmlst") else []), 
+        AMR_NCBI, 
+        AMR_NCBI_SUMMARY,
+        ### Optional outputs depending on config anvio = true 
+        *(ANVIO_FASTA_REFORMATTED if config["run"].get("anvio") else []), 
+        *(ANVIO_GENE_CALLS if config["run"].get("anvio") else []),
+        *(ANVIO_ANNOTATION if config["run"].get("anvio") else []), 
+        *(ANVIO_DB if config["run"].get("anvio") else []), 
+        *(ANVIO_DONE_DB if config["run"].get("anvio") else []),
+        *(ANVIO_EXTERNAL_GENOMES_FILE if config["run"].get("anvio") else []),
+        *(ANVIO_STORAGE_DB if config["run"].get("anvio") else []), 
+        *(ANVIO_PANGENOME_DB if config["run"].get("anvio") else []), 
+        *(ANVIO_ANI if config["run"].get("anvio") else []), 
+        ### Optional outputs depending on config snippy = true 
+        *(VC_REFG_GBK if config["run"].get("snippy") else []),
+        *(VC_SNIPPY if config["run"].get("snippy") else []),
+        *(VC_COPY_VCF if config["run"].get("snippy") else []), 
+        *(VC_COPY_VCF_GZ if config["run"].get("snippy") else []),
+        *(VC_SNIPPY_CORE if config["run"].get("snippy") else []),
+        *(VC_CORE_TREE if config["run"].get("snippy") else []),
+        *(VC_VCF_HEATMAP if config["run"].get("snippy_vcf_heatmap") else []),
+        *(VC_SNP_DISTANCE if config["run"].get("snippy") else []),
+        ### Optional outputs depending on config gwas = true
+        *(CSI if config["run"].get("gwas") else []), 
+        *(VCF_MERGED if config["run"].get("gwas") else []),
+        *(PHYLO_DIST if config["run"].get("gwas") else []),
+        BAM,
     message:
         "The genome analysis pipeline finished successfully!"
